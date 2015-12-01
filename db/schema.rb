ea37597516b9c39bcf92b2ac6839a5ea505e52a1
Noah Wells <noah16077@yahoo.com>
2015-11-30 21:53:38 -0500

    Hope this works

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126185455) do
+ActiveRecord::Schema.define(version: 20151126173356) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -59,7 +59,6 @@ ActiveRecord::Schema.define(version: 20151126185455) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "email"
   end
 
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
